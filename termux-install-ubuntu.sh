e84f956bc71d9f6d22abe5fdbcfdb7b92ddb0a79
AlanHee <alan.hee@outlook.com>
2024-02-04 02:32:32 +0800

    Added termux-install-ubuntu script

--- termux-install-ubuntu.sh
+++ termux-install-ubuntu.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source funs.sh
-yesOrNo "Install ubuntu in termux?(y/n)" "pkg install proot-distro"
+function installUbuntu() {
+	pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+}
+yesOrNo "Install ubuntu in termux?(y/n)" installUbuntu
