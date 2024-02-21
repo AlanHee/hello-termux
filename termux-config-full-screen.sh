e87e970f80cc591344f81af93fef364af7436166
AlanHee <alan.hee@outlook.com>
2024-02-22 00:49:18 +0800

    Update config full screen issue

--- termux-config-full-screen.sh
+++ termux-config-full-screen.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 source funs.sh
 
+function save() {
+	cat <<EOF >~/.termux/termux.properties
+extra-keys-style = none 
+fullscreen = true
+extra-keys = []
+disable-terminal-session-change-toast = true
+volume-keys = volume
+terminal-margin-horizontal = 0
+EOF
+	echo 'Updated termux.properties.'
+}
+
 function _() {
 	mv -f ~/.termux/termux.properties ~/.termux/termux.properties.old.$(date +%Y.%m.%d-%H:%M:%S)
-	cp -f $SRC/profile/termux.properties ~/.termux
-	echo 'Changes shell in full mode'
+	save
 }
 
 if [ -e ~/.termux/termux.properties ]; then
 	yesOrNo "Override existed termux.properties?(y/n) :" _
 else
-	cp -f $SRC/profile/termux.properties ~/.termux
-	echo 'Changes shell in full mode'
+	save
 fi
