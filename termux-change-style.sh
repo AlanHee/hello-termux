d5349e2e7e6be6a7299595d631a3aa6411fb5f49
AlanHee <alan.hee@outlook.com>
2024-05-03 05:59:50 +0800

    Fri

--- termux-change-style.sh
+++ termux-change-style.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-cd $HOME
-git clone https://github.com/adi1090x/termux-style
-cd termux-style
-./install
-# Run termux-style to change termux style:)
+#Determine if installed
+#Run termux-style to change termux style:)
+if type termux-style  &>/dev/null; then
+	termux-style
+else
+	cd $HOME
+	git clone https://github.com/adi1090x/termux-style
+	cd termux-style
+	./install
+	termux-style
+fi
