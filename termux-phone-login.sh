b1ef0ddcb1cd735f02b24a0d1dfa79300f6a5d7f
AlanHee <alan.hee@outlook.com>
2024-05-12 04:17:42 +0800

    Sun

---
+++ termux-phone-login.sh
@@ -0,0 +1,1 @@
+ssh u0_a141@$PHONE_IP -p 8022
--- termux-sshd-setup.sh
+++ termux-sshd-setup.sh
@@ -4,12 +4,24 @@ if [ "$(uname -o)" = "Android" ]; then
 	apt upgrade -qyy
 	termux-setup-storage
 	pkg install openssh
+	#you need a password for login
+	passwd
 	sshd
+	
+	#Test if setup
+	#pkill -f sshd
+	#sshd
+	#ssh `whoani`@127.0.0.1 -p 8022 
+
+	#auto run sshd while termux open
+	#echo "sshd" >.profile
+
 	echo 'sshd setup and runing.'
 	echo 'client setup flow:'
 	echo '1 gen pub key with config-ssh-keygen'
 	echo '2 cat id_rsa.pub >> authorized_keys'
 	echo '3 Find out the termux user and IP `whoami` `ifconfig -a`'
+	echo '4 login eg: ssh u0_a141@$xxx.xxx.x.x.x -p 8022'
 else
 	echo "For termux only"
 fi
