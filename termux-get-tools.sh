500a84839715fc70899b649bba5492816f43925b
AlanHee <alan.hee@outlook.com>
2024-11-27 12:23:40 +0800

    Wed

--- termux-get-tools.sh
+++ termux-get-tools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source funs.sh
-yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git python3 vim-python dart shfmt"
+# yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git python3 vim-python dart shfmt"
+yesOrNo "Install ssh vim shfmt dart?(y/n):" "apt install -y openssh git vim dart shfmt"
