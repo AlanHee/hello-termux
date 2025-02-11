9a5bbc811d802e7f2850536b4e14c8c6223a9a1f
AlanHee <alan.hee@outlook.com>
2025-02-12 01:19:12 +0800

    Wed

---
+++ termux-crond-setup.sh
@@ -0,0 +1,4 @@
+# * * * * * /data/data/com.termux/files/usr/bin/bash /data/data/com.termux/files/home/script.sh
+# * * * * * /data/data/com.termux/files/usr/bin/bash /data/data/com.termux/files/home/script.sh >> output.log 2>&1
+# 48h
+# 0 0 */2 * * /data/data/com.termux/files/usr/bin/bash /data/data/com.termux/files/home/bin/src/utils/sync-music-phone-to-server.sh >>music-sync.log 2>&1
--- termux-get-tools.sh
+++ termux-get-tools.sh
@@ -2,4 +2,4 @@
 
 source funs.sh
 # yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git python3 vim-python dart shfmt"
-yesOrNo "Install ssh vim shfmt dart?(y/n):" "apt install -y openssh git vim dart shfmt"
+yesOrNo "Install ssh vim cron shfmt dart?(y/n):" "apt install -y openssh git vim cronie dart shfmt"
